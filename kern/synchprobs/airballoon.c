83a13ae6ad5ef9b65176e52e3081ab7605fcc087
Stella Wang <stella01wang@gmail.com>
2021-10-06 12:11:15 -0700

    complete dandelion and marigold

--- kern/synchprobs/airballoon.c
+++ kern/synchprobs/airballoon.c
@@ -15,6 +15,7 @@ static int ropes_left = NROPES;
 
 static int hooks[NROPES];
 static int stakes[NROPES];
+static bool ropes[NROPES];
 
 /* Synchronization primitives */
 
@@ -34,16 +35,17 @@ initRopePositions()
 	for (int i = 0 ; i < NROPES ; i++) {
         hooks[i] = i;
         stakes[i] = i;
+		ropes[i] = false;
 	}
 }
 
 
-static
-void
-cleanup()
-{
-	lock_destroy(mutexLock);
-}
+// static
+// void
+// cleanup()
+// {
+// 	lock_destroy(mutexLock);
+// }
 
 
 // static
@@ -74,11 +76,17 @@ dandelion(void *p, unsigned long arg) // sky hooks
 
 		lock_acquire(mutexLock);
 		ropeIndex = hooks[hookIndex];
-		if (ropeIndex != -1) {
-			hooks[hookIndex] = -1;
-			kprintf("Dandelion severed rope %d\n", hookIndex);
+		if (ropeIndex != -1) { // hook has not been unhooked
+			if (ropes[ropeIndex] == false) { //rope is not yet severed 
+				ropes[ropeIndex] = true;
+				hooks[hookIndex] = -1;
+				kprintf("Dandelion severed rope %d\n", hookIndex);
 
-			ropes_left--;
+				ropes_left--;
+			} else { //update hook to be "unhooked" as the attached rope has been unstaked
+				hooks[hookIndex] = -1;
+			}
+			
 		}
 		lock_release(mutexLock);
 	}
@@ -99,15 +107,19 @@ marigold(void *p, unsigned long arg) // ground stakes
 
 	while (ropes_left > 0) {
 		int stakeIndex = random() % NROPES;
-		kprintf("%d", ropeIndex);
 
 		lock_acquire(mutexLock);
 		ropeIndex = stakes[stakeIndex];
-		if (ropeIndex != -1) {
-			stakes[stakeIndex] = -1;
-			kprintf("Marigold severed rope %d from stake %d\n", ropeIndex, stakeIndex);
+		if (ropeIndex != -1) { //stake has not been unstaked
+			if (ropes[ropeIndex] == false) { //rope is not yet severed
+				ropes[ropeIndex] = true;
+				stakes[stakeIndex] = -1;
+				kprintf("Marigold severed rope %d from stake %d\n", ropeIndex, stakeIndex);
 
-			ropes_left--;
+				ropes_left--;
+			} else { //update stake to be "unstaked" as the attached rope has been unhooked
+				stakes[stakeIndex] = -1;
+			}
 		}
 		lock_release(mutexLock);
 	}
@@ -116,29 +128,29 @@ marigold(void *p, unsigned long arg) // ground stakes
 	thread_exit();
 }
 
-static
-void
-flowerkiller(void *p, unsigned long arg)
-{
-	(void)p;
-	(void)arg;
+// static
+// void
+// flowerkiller(void *p, unsigned long arg)
+// {
+// 	(void)p;
+// 	(void)arg;
 
-	kprintf("Lord FlowerKiller thread starting\n");
+// 	kprintf("Lord FlowerKiller thread starting\n");
 
-	thread_exit();
-}
+// 	thread_exit();
+// }
 
-static
-void
-balloon(void *p, unsigned long arg)
-{
-	(void)p;
-	(void)arg;
+// static
+// void
+// balloon(void *p, unsigned long arg)
+// {
+// 	(void)p;
+// 	(void)arg;
 
-	kprintf("Balloon thread starting\n");
+// 	kprintf("Balloon thread starting\n");
 
-	thread_exit();
-}
+// 	thread_exit();
+// }
 
 
 // Change this function as necessary
@@ -146,7 +158,7 @@ int
 airballoon(int nargs, char **args)
 {
 
-	int err = 0, i;
+	int err = 0;
 
 	(void)nargs;
 	(void)args;
@@ -154,7 +166,13 @@ airballoon(int nargs, char **args)
 
 
 	initRopePositions();
+	ropes_left = NROPES;
 	mutexLock = lock_create("ropePositions");
+	if (mutexLock == NULL) {
+		kprintf("mutexLock Null");
+		panic("Could not create mutex_lock\n");
+	}
+
 
 	err = thread_fork("Marigold Thread",
 			  NULL, marigold, NULL, 0);
@@ -166,15 +184,15 @@ airballoon(int nargs, char **args)
 	if(err)
 		goto panic;
 
-	for (i = 0; i < N_LORD_FLOWERKILLER; i++) {
-		err = thread_fork("Lord FlowerKiller Thread",
-				  NULL, flowerkiller, NULL, 0);
-		if(err)
-			goto panic;
-	}
+	// for (i = 0; i < N_LORD_FLOWERKILLER; i++) {
+	// 	err = thread_fork("Lord FlowerKiller Thread",
+	// 			  NULL, flowerkiller, NULL, 0);
+	// 	if(err)
+	// 		goto panic;
+	// }
 
-	err = thread_fork("Air Balloon",
-			  NULL, balloon, NULL, 0);
+	// err = thread_fork("Air Balloon",
+	// 		  NULL, balloon, NULL, 0);
 	if(err)
 		goto panic;
 
@@ -184,6 +202,6 @@ panic:
 	      strerror(err));
 
 done:
-	cleanup();
+	// cleanup();
 	return 0;
 }
--- kern/thread/synch.c
+++ kern/thread/synch.c
@@ -47,18 +47,18 @@
 struct semaphore *
 sem_create(const char *name, unsigned initial_count)
 {
-        struct semaphore *sem;
+	struct semaphore *sem;
 
-        sem = kmalloc(sizeof(struct semaphore));
-        if (sem == NULL) {
-                return NULL;
-        }
+	sem = kmalloc(sizeof(struct semaphore));
+	if (sem == NULL) {
+			return NULL;
+	}
 
-        sem->sem_name = kstrdup(name);
-        if (sem->sem_name == NULL) {
-                kfree(sem);
-                return NULL;
-        }
+	sem->sem_name = kstrdup(name);
+	if (sem->sem_name == NULL) {
+			kfree(sem);
+			return NULL;
+	}
 
 	sem->sem_wchan = wchan_create(sem->sem_name);
 	if (sem->sem_wchan == NULL) {
@@ -68,39 +68,39 @@ sem_create(const char *name, unsigned initial_count)
 	}
 
 	spinlock_init(&sem->sem_lock);
-        sem->sem_count = initial_count;
+	sem->sem_count = initial_count;
 
-        return sem;
+	return sem;
 }
 
 void
 sem_destroy(struct semaphore *sem)
 {
-        KASSERT(sem != NULL);
+	KASSERT(sem != NULL);
 
 	/* wchan_cleanup will assert if anyone's waiting on it */
 	spinlock_cleanup(&sem->sem_lock);
 	wchan_destroy(sem->sem_wchan);
-        kfree(sem->sem_name);
-        kfree(sem);
+	kfree(sem->sem_name);
+	kfree(sem);
 }
 
 void
 P(struct semaphore *sem)
 {
-        KASSERT(sem != NULL);
+	KASSERT(sem != NULL);
 
-        /*
-         * May not block in an interrupt handler.
-         *
-         * For robustness, always check, even if we can actually
-         * complete the P without blocking.
-         */
-        KASSERT(curthread->t_in_interrupt == false);
+	/*
+	 * May not block in an interrupt handler.
+	 *
+	 * For robustness, always check, even if we can actually
+	 * complete the P without blocking.
+	 */
+	KASSERT(curthread->t_in_interrupt == false);
 
 	/* Use the semaphore spinlock to protect the wchan as well. */
 	spinlock_acquire(&sem->sem_lock);
-        while (sem->sem_count == 0) {
+	while (sem->sem_count == 0) {
 		/*
 		 *
 		 * Note that we don't maintain strict FIFO ordering of
@@ -114,21 +114,21 @@ P(struct semaphore *sem)
 		 * ordering?
 		 */
 		wchan_sleep(sem->sem_wchan, &sem->sem_lock);
-        }
-        KASSERT(sem->sem_count > 0);
-        sem->sem_count--;
+	}
+	KASSERT(sem->sem_count > 0);
+	sem->sem_count--;
 	spinlock_release(&sem->sem_lock);
 }
 
 void
 V(struct semaphore *sem)
 {
-        KASSERT(sem != NULL);
+	KASSERT(sem != NULL);
 
 	spinlock_acquire(&sem->sem_lock);
 
-        sem->sem_count++;
-        KASSERT(sem->sem_count > 0);
+	sem->sem_count++;
+	KASSERT(sem->sem_count > 0);
 	wchan_wakeone(sem->sem_wchan, &sem->sem_lock);
 
 	spinlock_release(&sem->sem_lock);
@@ -141,18 +141,18 @@ V(struct semaphore *sem)
 struct lock *
 lock_create(const char *name)
 {
-        struct lock *lock;
+	struct lock *lock;
 
-        lock = kmalloc(sizeof(struct lock));
-        if (lock == NULL) {
-                return NULL;
-        }
+	lock = kmalloc(sizeof(struct lock));
+	if (lock == NULL) {
+		return NULL;
+	}
 
-        lock->lk_name = kstrdup(name);
-        if (lock->lk_name == NULL) {
-                kfree(lock);
-                return NULL;
-        }
+	lock->lk_name = kstrdup(name);
+	if (lock->lk_name == NULL) {
+		kfree(lock);
+		return NULL;
+	}
 
 	lock->lk_wchan = wchan_create(lock->lk_name);
 	if (lock->lk_wchan == NULL) {
@@ -163,33 +163,33 @@ lock_create(const char *name)
 	spinlock_init(&lock->lk_lock);
 	lock->lk_holder = NULL;
 
-        return lock;
+	return lock;
 }
 
 void
 lock_destroy(struct lock *lock)
 {
-        KASSERT(lock != NULL);
+	KASSERT(lock != NULL);
 
 	KASSERT(lock->lk_holder == NULL);
 	spinlock_cleanup(&lock->lk_lock);
 	wchan_destroy(lock->lk_wchan);
 
-        kfree(lock->lk_name);
-        kfree(lock);
+	kfree(lock->lk_name);
+	kfree(lock);
 }
 
 void
 lock_acquire(struct lock *lock)
 {
 	DEBUGASSERT(lock != NULL);
-        KASSERT(curthread->t_in_interrupt == false);
+	KASSERT(curthread->t_in_interrupt == false);
 
 	spinlock_acquire(&lock->lk_lock);
 	KASSERT(lock->lk_holder != curthread);
 	while (lock->lk_holder != NULL) {
 		/* As in the semaphore. */
-                wchan_sleep(lock->lk_wchan, &lock->lk_lock);
+		wchan_sleep(lock->lk_wchan, &lock->lk_lock);
 	}
 
 	lock->lk_holder = curthread;
@@ -219,7 +219,7 @@ lock_do_i_hold(struct lock *lock)
 	ret = (lock->lk_holder == curthread);
 	spinlock_release(&lock->lk_lock);
 
-        return ret;
+	return ret;
 }
 
 ////////////////////////////////////////////////////////////
@@ -230,18 +230,18 @@ lock_do_i_hold(struct lock *lock)
 struct cv *
 cv_create(const char *name)
 {
-        struct cv *cv;
+	struct cv *cv;
 
-        cv = kmalloc(sizeof(struct cv));
-        if (cv == NULL) {
-                return NULL;
-        }
+	cv = kmalloc(sizeof(struct cv));
+	if (cv == NULL) {
+		return NULL;
+	}
 
-        cv->cv_name = kstrdup(name);
-        if (cv->cv_name==NULL) {
-                kfree(cv);
-                return NULL;
-        }
+	cv->cv_name = kstrdup(name);
+	if (cv->cv_name==NULL) {
+		kfree(cv);
+		return NULL;
+}
 
 	cv->cv_wchan = wchan_create(cv->cv_name);
 	if (cv->cv_wchan == NULL) {
@@ -251,19 +251,19 @@ cv_create(const char *name)
 	}
 
 	spinlock_init(&cv->cv_wchanlock);
-        return cv;
+	return cv;
 }
 
 void
 cv_destroy(struct cv *cv)
 {
-        KASSERT(cv != NULL);
+	KASSERT(cv != NULL);
 
 	spinlock_cleanup(&cv->cv_wchanlock);
 	wchan_destroy(cv->cv_wchan);
 
-        kfree(cv->cv_name);
-        kfree(cv);
+	kfree(cv->cv_name);
+	kfree(cv);
 }
 
 void
