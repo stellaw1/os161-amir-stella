f3686ea108bcc5c4a0621bac621fd0f00322506a
Stella Wang <stella01wang@gmail.com>
2021-10-14 18:04:56 -0700

    reconfigure kernel to use DUMBVM

--- kern/compile/DUMBVM/.depend.main.c
+++ kern/compile/DUMBVM/.depend.main.c
@@ -4,12 +4,12 @@ main.o: ../../main/main.c ../../include/types.h \
  ../../include/kern/reboot.h ../../include/kern/unistd.h \
  ../../include/lib.h ../../include/cdefs.h opt-noasserts.h \
  ../../include/spl.h ../../include/clock.h ../../include/kern/time.h \
- ../../include/thread.h ../../include/array.h ../../include/spinlock.h \
- includelinks/machine/spinlock.h ../../include/threadlist.h \
- includelinks/machine/thread.h ../../include/setjmp.h \
- includelinks/kern/machine/setjmp.h ../../include/proc.h \
- ../../include/current.h includelinks/machine/current.h \
- ../../include/synch.h ../../include/vm.h includelinks/machine/vm.h \
- ../../include/mainbus.h ../../include/vfs.h ../../include/device.h \
- ../../include/syscall.h ../../include/test.h ../../include/version.h \
- autoconf.h
+ opt-synchprobs.h ../../include/thread.h ../../include/array.h \
+ ../../include/spinlock.h includelinks/machine/spinlock.h \
+ ../../include/threadlist.h includelinks/machine/thread.h \
+ ../../include/setjmp.h includelinks/kern/machine/setjmp.h \
+ ../../include/proc.h ../../include/current.h \
+ includelinks/machine/current.h ../../include/synch.h ../../include/vm.h \
+ includelinks/machine/vm.h ../../include/mainbus.h ../../include/vfs.h \
+ ../../include/device.h ../../include/syscall.h ../../include/test.h \
+ ../../include/version.h autoconf.h
--- kern/compile/DUMBVM/.depend.thread.c
+++ kern/compile/DUMBVM/.depend.thread.c
@@ -10,4 +10,5 @@ thread.o: ../../thread/thread.c ../../include/types.h \
  ../../include/threadprivate.h ../../include/proc.h \
  ../../include/current.h includelinks/machine/current.h \
  ../../include/synch.h ../../include/addrspace.h ../../include/vm.h \
- opt-dumbvm.h ../../include/mainbus.h ../../include/vnode.h
+ opt-dumbvm.h ../../include/mainbus.h ../../include/vnode.h \
+ opt-synchprobs.h
