4ea6b9f3ff8ed70a45614520a187bebf5ee3e0f2
Stella Wang <stella01wang@gmail.com>
2021-10-14 18:03:42 -0700

    merged instructor test code

--- defs.mk
+++ defs.mk
@@ -13,7 +13,7 @@
 # The things that can be set here are documented in mk/os161.config.mk.
 #
 
-OSTREE=/root/os161/root
+OSTREE=/home/stella/os161/root
 PLATFORM=sys161
 MACHINE=mips
 COMPAT_CFLAGS= -DNEED_NTOHLL
