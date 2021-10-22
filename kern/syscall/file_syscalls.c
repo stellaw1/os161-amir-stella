e8002d56d98e6570500dd7ad305ec293b63592b3
Stella Wang <stella01wang@gmail.com>
2021-10-22 13:36:44 -0700

    open init code

---
+++ kern/syscall/file_syscalls.c
@@ -0,0 +1,35 @@
+#include <types.h>
+#include <kern/errno.h>
+#include <kern/syscall.h>
+#include <lib.h>
+#include <mips/trapframe.h>
+#include <thread.h>
+#include <current.h>
+#include <syscall.h>
+#include <synch.h>
+
+int open(const char *filename, int flags, mode_t mode) {
+
+    struct vnode **ret;
+    struct open_file *of;
+    int result;
+
+    of = kmalloc(sizeof(struct open_file));
+
+    result = vfs_open(filename, flags, mode, ret);
+
+    of->vn = ret;
+    of->offset = 0;
+    of->flags = flags;
+
+    lock_acquire(curproc->oft->table_lock);
+    for (int i = 0; i < OPEN_MAX; i++) {
+        if (curproc->oft[i] == NULL) {
+            curproc->oft[i] = of;
+            lock_release(curproc->oft->table_lock);
+            return result;
+        }
+    }
+    lock_release(curproc->oft->table_lock);
+    return EMFILE;
+}
