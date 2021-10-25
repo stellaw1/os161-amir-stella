3ef4a1aef45329dcbf8ad27bd04d71d00c39f3fa
Amir Ali Barkam <amirbarkam6@gmail.com>
2021-10-25 15:39:59 -0700

    Update lseek prototype

--- kern/include/file_syscalls.h
+++ kern/include/file_syscalls.h
@@ -12,6 +12,6 @@ int open(const_userptr_t filename, int flags, mode_t mode, int *retval);
 int close(int fd);
 ssize_t read(int fd, userptr_t buf, size_t buflen);
 ssize_t write(int fd, const_userptr_t buf, size_t nbytes);
-off_t lseek(int fd, off_t pos, int whence);
+off_t lseek(int fd, off_t pos, int whence, int32_t *retval, int32_t *retval_v1);
 
 #endif
