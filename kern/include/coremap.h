914a3af766a903c321be91ccd81ca2de089d317d
Stella Wang <46392676+stellaw1@users.noreply.github.com>
2021-12-03 19:09:12 -0800

    done (#17)
    * init
    
    * made alloc 1 page function
    
    * km1 & km2
    
    * passes on 512KB km1/2/3

--- kern/include/coremap.h
+++ kern/include/coremap.h
@@ -3,12 +3,16 @@
 #include <addrspace.h>
 
 
+bool coremap_initialized = false;
+
+
 /*
  * coremap definitions
  */
 struct coremap_entry *coremap;                                      /* global coremap pointer */
 static struct spinlock coremap_lock = SPINLOCK_INITIALIZER;        /* coremap lock */
 
+paddr_t coremap_addr;
 int totalpages;
 
 
@@ -16,9 +20,8 @@ int totalpages;
  * coremap entries definition
  */
 struct coremap_entry {
-	paddr_t address; // virtual page number; might need to make this an array to store myltiple virtual addresses
+	// paddr_t address; // virtual page number; might need to make this an array to store myltiple virtual addresses
 	vaddr_t virtual_addr; // virtual page number; might need to make this an array to store myltiple virtual addresses
-	// struct addrspace *mapped_space;
 
 	int numpages;
 
